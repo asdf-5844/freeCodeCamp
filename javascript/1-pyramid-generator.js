cff9977d359e3fd0b1c3a3645059069198f4b131
beneben <bchen5844@gmail.com>
2025-02-17 10:13:45 -0500

    Update 1-pyramid-generator.js

--- javascript/1-pyramid-generator.js
+++ javascript/1-pyramid-generator.js
@@ -1,3 +1,28 @@
-while (rows.length < count) {
-  rows.push(padRow(rows.length + 1, count));
+const character = "!";
+const count = 10;
+const rows = [];
+let inverted = false;
+
+function padRow(rowNumber, rowCount) {
+  return " ".repeat(rowCount - rowNumber) + character.repeat(2 * rowNumber - 1) + " ".repeat(rowCount - rowNumber);
 }
+
+for (let i = 1; i <= count; i++) {
+  if (inverted) {
+    rows.unshift(padRow(i, count));
+  } else {
+    rows.push(padRow(i, count));
+  }
+}
+
+/*while (rows.length < count) {
+  rows.push(padRow(rows.length + 1, count));
+}*/
+
+let result = ""
+
+for (const row of rows) {
+  result = result + row + "\n";
+}
+
+console.log(result);
