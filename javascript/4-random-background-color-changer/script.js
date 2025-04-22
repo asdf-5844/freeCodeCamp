1b35646224a2a0eceb8f4d62568dc6a8e6cadae3
beneben <bchen5844@gmail.com>
2025-04-22 19:02:31 -0400

    Update script.js

--- javascript/4-random-background-color-changer/script.js
+++ javascript/4-random-background-color-changer/script.js
@@ -1,3 +1,4 @@
+// Array holding the colors
 const darkColorsArr = [
   "#2C3E50",
   "#34495E",
@@ -11,20 +12,26 @@ const darkColorsArr = [
   "#800020",
 ];
 
+// Gets a random index for the array
 function getRandomIndex() {
   const randomIndex = Math.floor(darkColorsArr.length * Math.random());
   return randomIndex;
 }
 
+// Getting elements from DOM
 const body = document.querySelector("body");
 const bgHexCodeSpanElement = document.querySelector("#bg-hex-code");
 
-function changeBackgroundColor() {
-  const color = darkColorsArr[getRandomIndex()];
 
+function changeBackgroundColor() {
+  // Gets random color from array
+  const color = darkColorsArr[getRandomIndex()];
+  // Changes the text to the name of the color
   bgHexCodeSpanElement.innerText = color;
+  // Changes the background color
   body.style.backgroundColor = color;
 }
-const btn = document.querySelector("#btn");
 
+const btn = document.querySelector("#btn");
+// Adds this functionality to the button
 btn.onclick = changeBackgroundColor;
